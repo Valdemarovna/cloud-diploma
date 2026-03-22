6938c5fea60c733aad680211dcec231d430965dd
Valdemarovna <147051721+Valdemarovna@users.noreply.github.com>
2026-03-22 19:48:15 +0300

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import axios from "axios";
 
+axios.defaults.withCredentials = true;
 export const store = configureStore({
   reducer: {
     auth: authReducer,
