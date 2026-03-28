09c4d19edf672be18175d1d94c8524b79366223b
Valdemarovna <147051721+Valdemarovna@users.noreply.github.com>
2026-03-28 11:29:58 +0300

    anothe one wipe to deal

--- users/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    full_name = models.CharField(max_length=255)
-    storage_path = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.username
--- utils/csrf.py
+++
@@ -1,5 +0,0 @@
-from django.middleware.csrf import CsrfViewMiddleware
-
-class CsrfExemptSessionAuthentication:
-    def enforce_csrf(self, request):
-        return
