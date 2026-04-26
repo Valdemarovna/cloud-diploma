f88e0044cbc52d8a49897b98e853d390c0a26b1a
Valdemarovna <147051721+Valdemarovna@users.noreply.github.com>
2026-04-26 11:52:18 +0300

    fix

--- cloud_storage_backend/cloud_storage_backend/settings.py
+++ cloud_storage_backend/cloud_storage_backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == False
+DEBUG = os.getenv("DEBUG") == True
 
 #ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "89.104.69.130").split(",")
